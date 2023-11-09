50fa6fae6119155632f593cc87b126bdd424732b
chiara1911 <chiaracesari89@gmail.com>
2023-11-09 17:23:10 +0100

    add main

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,15 +1,16 @@
 <template>
-  
+  <div class="container mt-5">  
    <div class="d-flex justify-content-between">
-    <div>
-        <img src="../assets/images/dc-logo.png" alt="">
+        <div>
+            <img src="../assets/images/dc-logo.png" alt="">
+        </div>
+        <div>
+            <ul class="d-flex flex-row justify-content-between ">
+                <li v-for ="navLink in navLinks" class="p-3 ">{{ navLink}}</li>
+            </ul>
+        </div>
     </div>
-<div>
-   <ul class="d-flex flex-row justify-content-between ">
-    <li v-for ="navLink in navLinks" class="p-3 ">{{ navLink}}</li>
-   </ul>
-</div>
-</div>
+    </div>
 </template>
 
 <script>
--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,15 +1,60 @@
 <template>
-   <h2>main</h2>
+    <div class="container">
+<div id="top-main" class="bg-dark">
+    <h2 class="text-light py-5"> -- > Content Goes here  -- </h2>
+</div>
+<div id="bottom-main">
+    <ul class="d-flex flex-row justify-content-between ">
+                <li v-for ="(el,index) in items" class="p-3 ">
+                    <img :src="el.img" alt="">
+                    <span>{{el.text}}</span></li>
+    </ul>
+</div>
+    
+    <div class="p-5">
+        
+    </div>
+</div>
 </template>
 
 <script>
     export default {
-        name: 'MainApp'
+        name: 'MainApp',
+        data(){
+            return {
+                items:[
+                    {
+                        img: "/src/assets/images/buy-comics-digital-comics.png",
+                        text: "DIGITAL COMICS"
+                    },
+                    {
+                        img: "/src/assets/images/buy-comics-merchandise.png",
+                        text: "dc merchandise"
+                    },
+                    {
+                        img: "/src/assets/images/buy-comics-subscriptions.png",
+                        text: "subcription"
+                    },
+                    {
+                        img: "/src/assets/images/buy-comics-shop-locator.png",
+                        text: "comic shop locator"
+                    },
+
+                ]
+            }
+        }
+   
+
+    
     }
-</script>
+    </script>
 
 <style lang="scss">
-
-background-color: black;
+    #top-main{
+    background-color: black;
+   }
+    #bottom-main{
+    background-color: blue;
+   }
 
 </style>
