f5992cabd6624d25dd05544022f528f58953d5da
chiara1911 <chiaracesari89@gmail.com>
2023-11-09 15:52:44 +0100

    ticket

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,11 +1,34 @@
 <template>
    <h2>header</h2>
+
+   <ul class="d-flex flex-row flex-nowrap">
+    <li v-for ="navLink in navLinks">{{ navLink}}</li>
+   </ul>
 </template>
 
 <script>
     export default {
-        name: 'HeaderApp'
-    }
+        name: 'HeaderApp',
+
+        data(){
+            return {
+                navLinks :[               
+                'characters',
+                'comics',
+                'movies',
+                'tv',
+                'games',
+                'collectibles',
+                'videos',
+                'fans',
+                'news',
+                'shop'
+                ],
+            }
+        }
+    } 
+
+    
 </script>
 
 <style lang="scss" scoped>
