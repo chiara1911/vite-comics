b7fe6b7e9a8abcb118b033b1eb45b2a570c74363
chiara1911 <chiaracesari89@gmail.com>
2023-11-09 15:21:36 +0100

    add scaffolding

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,13 @@
+<template>
+   <h2>header</h2>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderApp'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/MainApp.vue
@@ -0,0 +1,13 @@
+<template>
+   <h2>main</h2>
+</template>
+
+<script>
+    export default {
+        name: 'MainApp'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
